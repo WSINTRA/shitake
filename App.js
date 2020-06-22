a6d9293231aaa8d2bf3081d2e3b2b2613bc6ea0e
WS <ws@WSs-MacBook-Pro.local>
2020-06-22 14:11:03 -0400

    NavRoot created for Login and Signup, included a splash page

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { WelcomeStyle } from "./styles/global";
-import Welcome from './components/auth-stack/welcome';
+import NavRoot from './components/auth-stack/navRoot';
 
 
 function HomeScreen() {
@@ -22,7 +22,8 @@ function App() {
         <HomeScreen/> 
         : 
         <View style={WelcomeStyle.container}>
-        <Welcome/> 
+        <NavRoot/> 
+        <Text>Welcome to codename:Shitake!</Text>
         </View>
         }
     </NavigationContainer>
