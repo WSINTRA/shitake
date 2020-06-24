1e8538d23c8a323ae4b142dcd7046516121749c4
WS <ws@WSs-MacBook-Pro.local>
2020-06-24 00:08:17 -0400

    Built out first draft for Login form style, added a splash load screen and installed icons for use on input forms

--- styles/global.js
+++ styles/global.js
@@ -1,14 +1,42 @@
 import { StyleSheet } from "react-native";
 import { Dimensions } from "react-native";
-import {eggShell } from './colors';
-const screenHeight = Math.round(Dimensions.get("window").height);
+import { eggShell, deepChestnut, charCoal } from "./colors";
+export const screenHeight = Math.round(Dimensions.get("window").height);
 export const WelcomeStyle = StyleSheet.create({
   container: {
-    //THIS BORDER IS TEMPORARY
-    //helps when plotting out the components
     backgroundColor: eggShell,
     height: screenHeight,
     flex: 1,
-    justifyContent: 'space-between',
+  },
+  loginFormWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    marginTop:100,
+  },
+  inputWrapper:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    width:'80%',
+    fontFamily: "GillSans-SemiBold",
+    borderColor: deepChestnut,
+    backgroundColor: eggShell,
+    padding: 10,
+    textAlign: 'center',
+    borderBottomWidth: 3,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    marginBottom:20,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: charCoal,
+    shadowOpacity: 0.5,
+    
+  },
+  input: {
+    paddingLeft:20,
+    width:"100%",
+    fontSize:24
   },
 });
