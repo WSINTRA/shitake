bf072ba6d4f98820b21a45bf2581196b091a594a
WSINTRA <will.simpson85@gmail.com>
2020-06-30 18:03:21 -0400

    Skeleton of nested components complete.

--- components/auth-stack/navRoot.js
+++ components/auth-stack/navRoot.js
@@ -2,27 +2,42 @@ import React, { useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./login";
 import Signup from "./signup";
-import Splash from './splash';
-import {androidGreen, deepChestnut, hunterGreen} from '../../styles/colors';
+import Splash from "./splash";
+import { deepChestnut } from "../../styles/colors";
 const NavRoot = (props) => {
   //   const [switchForm, setSwitchFrom] = useState(false);
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator initialRouteName="Splash" 
-    screenOptions={ {headerTransparent:true, headerTintColor:deepChestnut }}
-    >
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={
+        { 
+          headerTransparent: true, 
+          headerTintColor: deepChestnut 
+          }
+        }>
         <Stack.Screen 
-        name="Splash" 
-        options={ {headerShown:false} }
-        >
+          name="Splash" 
+          options={
+            { 
+              headerShown: false
+            }
+          }>
           {(props) => <Splash {...props} />}
         </Stack.Screen>
-        <Stack.Screen 
-        options={{headerBackTitle:"Welcome"}}
-        name="Login">
+
+        <Stack.Screen
+          name="Login" 
+          options={
+            { 
+              headerBackTitle: "Welcome" 
+              }
+          }>
           {(props) => <Login {...props} />}
         </Stack.Screen>
-        <Stack.Screen name="Signup">
+
+        <Stack.Screen 
+          name="Signup">
           {(props) => <Signup {...props} />}
         </Stack.Screen>
     </Stack.Navigator>
